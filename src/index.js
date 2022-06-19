7f54757a34f179ced740087ea62e1f74559eddbc
esadakman <esadd26@gmail.com>
2022-06-19 16:51:38 +0300

    reactstrap added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+// ! reactstap'i npm install reactstrap" komutuyla kurduktan sonra index.js'e gelip alt satırdaki gibi import ediyoruz
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
