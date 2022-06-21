de1a5a6cd88aaf1bf0fef5d8f1a53256baeeb0f9
esadakman <esadd26@gmail.com>
2022-06-21 09:06:24 +0300

    props notes added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import Navi from "./Navi";
 import ProductList from "./ProductList";
 // ? Başka bir componenti kullanmak için öncelikle import adiyoruz, ardından return içerisinde çağırıyoruz(Direkt return içindede <"___" /> sayesinde otomatik import edip kullanabiliriz)
 // ! Reactstrap'i kullanmak için import etmemiz gerekiyor
+// ! Bir componentten diğeine data taşımak için Props'u kullanırız
 import { Container, Row, Col } from "reactstrap";
 
 function App() {
+  // ! İstersek category ve product listimizi bir değişken içerisinde oluşturup, ordanda alabiliriz
+  // * Bu değişkenleri okumak için ise direkt title'ın içine gidip {} içerisinde değiikenimizi çağırabiliriz
+  let titleProduct = "Product List";
+  let titleCategory = "Category List";
   return (
     <div>
       {/* <h2>Hello React !!! </h2> */}
@@ -17,11 +22,14 @@ function App() {
           <Navi />
         </Row>
         <Row>
-          <Col xs="3" bg->
-            <CategoryList />
+          <Col xs="3">
+            {/* //* Datamızı taşımak datayı taşıyacağımız yere gelip title veririz. (title yerine başka bir isimde verebiliriz fakat diğer tarafta aynı isimle çağırmamız gerekir)  */}
+            {/* <CategoryList title="Category List" /> */}
+            <CategoryList title={titleCategory} />
           </Col>
           <Col xs="9">
-            <ProductList />
+            {/* <ProductList title="Product List" /> */}
+            <ProductList title={titleProduct} />
           </Col>
         </Row>
       </Container>
--- src/CategoryList.js
+++ src/CategoryList.js
@@ -2,10 +2,19 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
+  // ! App js 'deki category list'imi okumak için bir constructor oluşturuz ,
+  // ! ardından "super" diyerek props'ımızı component'imize taşıyoruz. en son ise state oluşturuyoruz
+  // ? en son ise çağırmak istediğimiz yere gidip this.props deyip title diyerek çağırıyoruz
+  constructor(props) {
+    super(props);
+    state: {
+    }
+  }
+
   render() {
     return (
       <div>
-        <h3>Category List</h3>
+        <h3>{this.props.title}</h3>
 
         <ListGroup>
           <ListGroupItem>Cras justo odio</ListGroupItem>
