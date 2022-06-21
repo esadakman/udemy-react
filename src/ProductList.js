07b4585da5d50c4612a787074823f0a2fddc199e
esadakman <esadd26@gmail.com>
2022-06-21 17:14:20 +0300

    encapsulation with props

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+// ! app.js'de encapsulation yöntemimizi uyguladıktan sonra burada divimizin içine girip {this.props.info.__değişkenismi__} şeklinde çağırıyoruz
 
 export default class ProductList extends Component {
   render() {
     return (
       <div>
-        <h3>Product List</h3>
+        <h3>{this.props.info.title}</h3>
+        <h3>{this.props.info.baskaBisey}</h3>
       </div>
     );
   }
