69df382a0728a3ccfb27f54ebc65f9ef0a01d653
esadakman <esadd26@gmail.com>
2022-06-21 17:59:05 +0300

    working with state

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -3,28 +3,36 @@ import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
   // ! App js 'deki category list'imi okumak için bir constructor oluşturuz ,
-  // ! ardından "super" diyerek props'ımızı component'imize taşıyoruz. en son ise state oluşturuyoruz
+  // ! ardından "super" diyerek props'ımızı component'imize taşıyoruz. en son ise state oluşturuyoruz. Sonra bu state'i {this.state.counter} şeklinde kullanıyoruz
   // ? en son ise çağırmak istediğimiz yere gidip this.props deyip title diyerek çağırıyoruz
-  // *=======================
-  // ! app.js'de encapsulation yöntemimizi uyguladıktan sonra burada divimizin içine girip {this.props.info.__değişkenismi__} şeklinde çağırıyoruz
   constructor(props) {
     super(props);
-    state: {
-    }
+    this.state = {
+      categories: [
+        { categoryId: 1, categoryName: "Beverages" },
+        { categoryId: 2, categoryName: "Condiments" },
+      ],
+    };
   }
+  // *=======================
+  // ! app.js'de encapsulation yöntemimizi uyguladıktan sonra burada divimizin içine girip {this.props.info.__değişkenismi__} şeklinde çağırıyoruz
 
   render() {
     return (
       <div>
         {/* <h3>{this.props.title}</h3> */}
         <h3>{this.props.info.title}</h3>
+        <h3>{this.state.counter}</h3>
 
         <ListGroup>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Dapibus ac </ListGroupItem>
-          <ListGroupItem>Morbi leo </ListGroupItem>
-          <ListGroupItem>{this.props.info.title}</ListGroupItem>
-          <ListGroupItem>Vestibulum </ListGroupItem>
+          {
+            // ! yukarıda yazmış olduğumuz categories'im içerisinde ki bilgileri getirmek için
+            this.state.categories.map((category) => (
+              <ListGroupItem key={category.categoryId}>
+                {category.categoryName}{" "}
+              </ListGroupItem>
+            ))
+          }
         </ListGroup>
       </div>
     );
